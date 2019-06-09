31ca2662462d02731b14854830d95c4f862eea99
sohlin <46018089+sohlin@users.noreply.github.com>
2019-06-09 16:11:54 +0800

    Update FreAnalysis_word.py

--- FreAnalysis_word.py
+++ FreAnalysis_word.py
@@ -2,7 +2,7 @@
 import os
 from collections import Counter
 # 假设要读取文件名为read，位于当前路径
-filename = 'abstract5.txt'
+filename = 'martinwolf.txt'
 # 当前进程工作目录
 dirname = os.getcwd()
 fname = os.path.join(dirname, filename)
@@ -12,19 +12,27 @@ with open(fname,encoding='utf-8') as f:
     for ch in '''!"@#$%^&*()+,-./:;<=>?@'[\\]_`~{|}''': #替换特殊字符
         s=s.replace(ch, ' ')
 counter = Counter(s.replace('\n', ' ').split(' '))
-# 格式化要输出的每行数据，尾占8位，首占18位
-def geshi(a, b):
-    return "{:<18}""{:>8}".format(a,b) + '\n'
-title = geshi('词', '频率')
+
+#替换停用词
+file = open('stopwords.txt','r',encoding='utf-8')
+for line in file.readlines():
+    curLine=line.strip().split(" ")
+
 results = []
-# 要输出的数据，每一行由：词、频率+'\n'构成，序号=List.index(element)+1
-for w, c in counter.most_common(300):
-    results.append((w, c))
+a=0
+for w, c in counter.most_common():
+    if w != '' and w != '   ' and w != ' -':
+        a+=c
+for w, c in counter.most_common():
+    if w != ''and w != '   ' and w not in curLine:
+        results.append((w, c/a*1000))
 sorted(results, key=lambda x: x[1], reverse=True)
 # 将统计结果写入文件write.txt中
 writefile = 'write1.txt'
 wpath = os.path.join(dirname, writefile)
 with open(wpath, 'w',encoding='utf-8') as f:
+    f.write('Word' + '                ' + 'Fre/‰')
+    f.write('\n')
     for h in results:
         f.write(h[0]+'                ')
         f.write(str(h[1]))
